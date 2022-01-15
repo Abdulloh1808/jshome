f8bfde4510356487b73a103f23931a0f02919d16
abdulloh <abu@gmail.com>
2022-01-15 18:01:25 +0500

    js7

--- js/script.js
+++ js/script.js
@@ -24,18 +24,18 @@
 // }
 // console.log(count);
 
-// let str = ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto minima corrupti a iusto veniam expedita repudiandae tenetur necessitatibus amet eligendi tempora accusamus impedit placeat /* qui unde, quasi nesciunt vel sit.'
+let str = ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto minima corrupti a iusto veniam expedita repudiandae tenetur necessitatibus amet eligendi tempora accusamus impedit placeat /* qui unde, quasi nesciunt vel sit.'
 
-// const bdr = str.split() 
-// // const unli=[ 'u','a','e','i','o']
-// const sym = ['', '.',',','?','!' ]
-// let count = 0
-// for (let i = 0; i < bdr.length; i++) {
-//         if (bdr[i]== 'u'|| bdr[i]=='a' || bdr[i]=='e' || bdr[i]=='i'|| bdr[i]=='o' ) {
-//             count ++
-//         }  
-// }
-// console.log(count);
+const bdr = str.split() 
+// const unli=[ 'u','a','e','i','o']
+const sym = ['', '.',',','?','!' ]
+let count = 0
+for (let i = 0; i < bdr.length; i++) {
+        if (bdr[i]== 'u'|| bdr[i]=='a' || bdr[i]=='e' || bdr[i]=='i'|| bdr[i]=='o' ) {
+            count ++
+        }  
+}
+console.log(count);
 
 // bdr.splice ( 1,0,str)
 
